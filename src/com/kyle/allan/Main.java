4d3a7a2a6855a27036fa283e236ae6991216dab6
Kyle Allan <97403651+Kyle-Allan@users.noreply.github.com>
2022-10-14 14:50:49 -0300

    not completed yet

--- src/com/kyle/allan/Main.java
+++ src/com/kyle/allan/Main.java
@@ -1,8 +1,46 @@
 package com.kyle.allan;
 
+import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// testing databases commit
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Filename?");
+        String fileName = scan.nextLine();
+        System.out.println("Path?");
+        String path = scan.nextLine();
+        readTextFile(path, fileName);
+
+
     }
+
+    static void readTextFile(String path, String fileName) {
+
+        try{
+            try(Scanner scanner = new Scanner(new File(path + fileName))){
+
+                while(scanner.hasNextLine()){
+                    int question = scanner.nextInt();
+                    String answer = scanner.nextLine();
+                    if(scanner.nextLine() == "$"){
+                        scanner.skip("$");
+                    }
+                }
+
+
+            }
+
+        }
+        catch(FileNotFoundException fnfe){
+            System.out.println("fnfe exception");
+        }
+
+    }
+
 }
+
