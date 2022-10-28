cbc3b26ae4b7265a1f7a4ae3422ff6c8eecfb199
Kyle Allan <97403651+Kyle-Allan@users.noreply.github.com>
2022-10-28 15:00:38 -0300

    insert exam and questions in that exam to databse

--- src/com/kyle/allan/Main.java
+++ src/com/kyle/allan/Main.java
@@ -10,29 +10,37 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try{
+            String connectionString = "jdbc:mysql://47.54.75.83:3306/oems";
+            Connection con = DriverManager.getConnection(connectionString, "test", "password");
+
+            if(con != null){
+
+                Scanner scan = new Scanner(System.in);
+                int instID = 1;
+                System.out.println("What is the exam name?");
+                String examName = scan.nextLine();
+                System.out.println("Do you want this exam to be open?\nType \"true\" for yes:\nType \"false\" for no:");
+                boolean isOpen = scan.nextBoolean();
+                String garbage = scan.nextLine();
+                System.out.println("What is the filename?");
+                String fileName = scan.nextLine();
+                System.out.println("What is the path?");
+                String path = scan.nextLine();
+                path = path.replace("\\","\\\\");
+                path = path.concat("\\");
+                createExam(path, fileName, examName, instID, isOpen,con);
 
 
+            }
 
-            Scanner scan = new Scanner(System.in);
-            int instID = 1;
-            System.out.println("What is the exam name?");
-            String examName = scan.nextLine();
-            System.out.println("Do you want this exam to be open?\nType \"true\" for yes:\nType \"false\" for no:");
-            boolean isOpen = scan.nextBoolean();
-            String garbage = scan.nextLine();
-            System.out.println("What is the filename?");
-            String fileName = scan.nextLine();
-            System.out.println("What is the path?");
-            String path = scan.nextLine();
-            path = path.replace("\\","\\\\");
-            path = path.concat("\\");
-            createExam(path, fileName, examName, instID, isOpen);
-
-
+        }catch(SQLException s){
+            System.out.println("sql exception");
+        }
 
     }
 
-    static void createExam(String path, String fileName, String examName, int instID, boolean isOpen){
+    static void createExam(String path, String fileName, String examName, int instID, boolean isOpen, Connection con){
 
         Integer questionNumber = null;
         String questionDescription = null;
@@ -44,7 +52,7 @@ public class Main {
             try{
                 try(Scanner fileScanner = new Scanner(new File(path + fileName))){
 
-                    insertExam(examName,instID,isOpen);
+                    insertExam(examName,instID,isOpen,con);
 
                     while(fileScanner.hasNextLine()){
                         Scanner lineScanner = new Scanner(fileScanner.nextLine());
@@ -54,6 +62,7 @@ public class Main {
                         answer = lineScanner.next();
                         Question question = new Question(questionNumber,questionDescription,answer);
                         exam.addToExam(question);
+                        insertQuestion(con,examName,questionDescription,answer);
 
                     }
 
@@ -70,11 +79,9 @@ public class Main {
 
     }
 
-    static void insertExam(String examName, int instID, boolean isOpen){
+    static void insertExam(String examName, int instID, boolean isOpen, Connection con){
 
         try{
-            String connectionString = "jdbc:mysql://47.54.75.83:3306/oems";
-            Connection con = DriverManager.getConnection(connectionString, "test", "password");
 
             PreparedStatement PrepStat = con.prepareStatement("INSERT into EXAM values (default, ?, ?, ?)");
             PrepStat.setString(1,examName);
@@ -87,6 +94,33 @@ public class Main {
             s.printStackTrace();
         }
 
+
+    }
+
+    static void insertQuestion(Connection con, String examName, String questionDescription, String questionAnswer){
+
+        try{
+
+
+
+            PreparedStatement PrepStatement  = con.prepareStatement("SELECT exam_id from EXAM where exam_name = ?", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            PrepStatement.setString(1,examName);
+            ResultSet ResSet = PrepStatement.executeQuery();
+            ResSet.first();
+            int exam_id = ResSet.getInt(1);
+
+
+            PreparedStatement PrepStat = con.prepareStatement("INSERT into QUESTION values(default, ?, ?, ?)");
+            PrepStat.setInt(1,exam_id);
+            PrepStat.setString(2,questionDescription);
+            PrepStat.setString(3,questionAnswer);
+            PrepStat.executeUpdate();
+
+
+        }catch(SQLException s){
+            System.out.println("sql exception");
+            s.printStackTrace();
+        }
 
     }
 
